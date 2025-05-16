0b4046a8871c24b66b72fb91d62b7c190ce1ab45
SorinArt99 <sorinartene999@yahoo.com>
2025-05-16 17:53:33 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-website-sr-2/precache-manifest.2c331182722fa5d623a2b8fc0954681f.js"
+  "/react-website-sr-2/precache-manifest.103c5e46e85aa4cdf97a4741e6fcf24a.js"
 );
 
 self.addEventListener('message', (event) => {
