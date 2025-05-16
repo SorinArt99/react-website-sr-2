7e3e490eab2d112188e5501a558bb75dff4c4b37
SorinArt99 <sorinartene999@yahoo.com>
2025-05-16 17:19:42 +0300

    Updates

---
+++ precache-manifest.75fbded49cfb81002fe71d157edae93f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a483a8752cc846fbc9d919f7469f3a",
+    "url": "/react-website-sr-2/index.html"
+  },
+  {
+    "revision": "35fcd39f5f61468055c8",
+    "url": "/react-website-sr-2/static/css/main.5202aacf.chunk.css"
+  },
+  {
+    "revision": "e4614a47116434c141a6",
+    "url": "/react-website-sr-2/static/js/2.539a0607.chunk.js"
+  },
+  {
+    "revision": "35fcd39f5f61468055c8",
+    "url": "/react-website-sr-2/static/js/main.668af403.chunk.js"
+  },
+  {
+    "revision": "ff8f00340a0a914ef222",
+    "url": "/react-website-sr-2/static/js/runtime~main.d9e57b04.js"
+  }
+]);
